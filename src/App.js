a5beb263cf4bed8f50f1c85f59453c1854b83c08
ductri <31211024561@student.isb.edu.vn>
2022-11-12 23:24:56 +0700

    BaiTapShoe

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import ProductList from "./Ex3/ProductList";
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <ProductList />
+      </div>
+    );
+  }
+}
+
+export default App;
